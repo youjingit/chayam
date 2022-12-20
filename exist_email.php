8e1fae834dbf434a013822a9646112213c6453b1
youjin <youjindev@gmail.com>
2022-12-20 18:18:30 +0900

    menu ing

---
+++ exist_email.php
@@ -0,0 +1,20 @@
+<?php
+// 데이터 가져오기
+$u_id = $_GET["email"];
+
+// DB 연결
+include "../inc/dbcon.php";
+
+// 쿼리 작성
+$sql = "select email from members where email='$email';";
+
+// 쿼리 전송
+$result = mysqli_query($dbcon, $sql);
+
+// DB에서 데이터 가져오기
+$num = mysqli_num_rows($result);
+
+mysqli_close($dbcon);
+
+echo $num ? 't' : 'f';
+?>
